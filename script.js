d08b82751ddec50a61e884187216e87200d87b3f
Zhenyapehenya <epsareva77@gmail.com>
2020-10-16 16:24:21 +0700

    Новая ветка

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 fetch('https://api.github.com/users/Zhenyapechenya')
     .then(res => res.json())
-    .then(json => console.log(json.name))
+    .then(json => console.log(json.bio))
     .catch(err => console.log(err));
