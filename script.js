3d1d5692106354202fc83c7d3df22fd8e812ffcd
Zhenyapehenya <epsareva77@gmail.com>
2020-10-16 18:08:58 +0700

    Вывела изображение профиля

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-fetch('https://api.github.com/users/Zhenyapechenya')
+let body = document.body;
+let url = 'https://api.github.com/users/Zhenyapechenya';
+let parameters = window.location.search;
+let username = parameters.slice(parameters.indexOf('=')+1);
+if (parameters !== '') {
+    url = `https://api.github.com/users/${username}`;
+}
+
+fetch(url)
     .then(res => res.json())
-    .then(json => console.log(json.bio))
-    .catch(err => console.log(err));
+    .then(json => {
+        console.log(json.avatar_url);
+        console.log(json.name);
+        console.log(json.bio);
+        console.log(json.html_url);
+
+        let img = new Image();   // Создает новый элемент img 
+        img.src = json.avatar_url;
+        body.append(img);
+
+        body.append(json.name);
+    })
+// fetch(url)
+//     .then(res => res.json())
+//     .then(json => console.log(json.bio))
+//     .catch(err => console.log(err));
